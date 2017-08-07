0f7c688cb2804b08fc979cfe83cbb51e5c7c9a0e
litian <1129511456@qq.com>
2017-08-07 10:07:20 +0800

    全部代码

--- app/src/main/java/com/administrator/litian20170807/MainActivity.java
+++ app/src/main/java/com/administrator/litian20170807/MainActivity.java
@@ -1,13 +1,36 @@
 package com.administrator.litian20170807;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements TopBar.TopBarClickListener {
+    private TopBar mTopBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.widget_topbar);
+
+       mTopBar = ((TopBar) findViewById(R.id.top_bar));
+        mTopBar.setTopBarClickListener(this);
+
     }
+
+    @Override
+    public void leftClick() {
+        Toast.makeText(getApplicationContext(),"点击返回",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void rightClick() {
+        Toast.makeText(getApplicationContext(),"点击了解更多",Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
